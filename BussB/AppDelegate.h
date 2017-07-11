702628bd4a34473c014e5aa12ad5dcd9f9a4d2c4
JanvenZhao <janvenzhao@gmail.com>
2017-07-11 08:57:47 +0800

    main project

---
+++ BussB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BussB
+//
+//  Created by Janven on 17/7/11.
+//  Copyright © 2017年 Janven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
